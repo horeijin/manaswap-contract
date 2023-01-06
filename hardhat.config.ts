d0696cb885c0bede41ef2a62d0a2501d96e8865d
horeijin <91329574+horeijin@users.noreply.github.com>
2023-01-06 12:56:19 +0900

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,12 +10,12 @@ const config: HardhatUserConfig = {
       gasPrice: 875000000,
     },
     goerli: {
-      url: 'https://ethereum-goerli-rpc.allthatnode.com/we41J8x3JX9RGVkYlmVnlKK61U4mPsnX', //all that node에서 만든 RPC노드 주소
-      accounts: ['f99a14e17cfe6172f3eaefcab5bd92328150e526c9ed4e3d76d1cce73ed6665c'] //배포에 사용할 메타마스크 개인키
+      url: 'RPC노드 주소',
+      accounts: ['배포에 사용할 메타마스크 개인키']
     },
   },
   etherscan: {
-    apiKey: "R8UZH3AI7UZVF269GUTF1WKY62AJHWS23T" //이더스캔(메인넷)에서 내가 발급받은 api키
+    apiKey: "이더스캔(메인넷)에서 발급받은 api키"
   }
 };
 
